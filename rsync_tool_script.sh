ee3cf709e14df9e1b4211755018c6c910e3e9a7d
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2025-03-07 20:48:13 +0530

    Update rsync_tool_script.sh

--- rsync_tool_script.sh
+++ rsync_tool_script.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#rsync tool script
+local_directory_path=/path/of/the/local/directory
+remote_user=name_of_the_remote_user
+remote_ip=ip_address
+remote_path=/path/of/the/file/of/remote/directory
+
+rsync -avrzh $local_directory_path $remote_user@$remote_ip:$remote_path
+
 
