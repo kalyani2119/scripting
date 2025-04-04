6155fcc7b004b9db0087f000d9ab757ca88c9bcc
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2025-04-04 07:14:23 +0530

    Add files via upload

---
+++ dma_messg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DMA_BUFFER="dma_buffer.txt"
+
+echo -e "\U1F30E" "Hello World" > "$DMA_BUFFER"
+
+#Print the message
+Message=$(cat "$DMA_BUFFER")
+echo "Received message:$Message"
+
+rm "$DMA_BUFFER"
